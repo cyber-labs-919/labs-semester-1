1ef4beedbfb423d2f7b86249adf37fba26306c44
cyber-labs-919 <92755096+cyber-labs-919@users.noreply.github.com>
2021-10-26 13:23:01 +0400

    Update matrixEnc.h

--- Lab_COMBINED/Lab_COMBINED/matrixEnc.h
+++ Lab_COMBINED/Lab_COMBINED/matrixEnc.h
@@ -7,7 +7,7 @@ class MatrixEncCrypto : public Crypto {
     int** makeMat(std::string str, int m);
     std::string rotateMat(int** mat, int m);
 public:
-    std::string encrypt(std::string input, map_type m=map_type()) override;
-    std::string decrypt(std::string input, map_type m = map_type()) override;
-    void test(std::string s, map_type options = map_type()) override;
+    virtual std::string encrypt(std::string input, map_type m=map_type());
+    virtual std::string decrypt(std::string input, map_type m = map_type());
+    virtual void test(std::string s, map_type options = map_type());
 };
