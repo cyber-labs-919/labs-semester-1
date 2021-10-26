e1d9278d2c1974f23907d77107c1918aa9e49cbc
cyber-labs-919 <92755096+cyber-labs-919@users.noreply.github.com>
2021-10-26 13:20:36 +0400

    Update rle.h

--- Lab_COMBINED/Lab_COMBINED/rle.h
+++ Lab_COMBINED/Lab_COMBINED/rle.h
@@ -4,7 +4,7 @@
 
 class RLECompressor : public Crypto {
 public:
-    std::string encrypt(std::string input, map_type m = map_type()) override;
-    std::string decrypt(std::string input, map_type m = map_type()) override;
-    void test(std::string s, map_type options = map_type()) override;
+    virtual std::string encrypt(std::string input, map_type m = map_type());
+    virtual std::string decrypt(std::string input, map_type m = map_type());
+    virtual void test(std::string s, map_type options = map_type());
 };
